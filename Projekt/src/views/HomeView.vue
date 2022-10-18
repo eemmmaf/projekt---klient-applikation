790e69a6a88fff5a2e9811305603cf7e6ba9ebc5
Emma Forslund <emfo2102@student.miun.se>
2022-10-18 23:33:32 +0200

    Skapat Vy för startsidan

---
+++ Projekt/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="md:container md:mx-auto">
+        <div>
+            <!--Komponent för artikel-->
+            <Boxes />
+        </div>
+    </div>
+</template>
+
+<script>
+import Boxes from '../components/Boxes.vue';
+
+
+export default {
+    components: {
+        Boxes
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>

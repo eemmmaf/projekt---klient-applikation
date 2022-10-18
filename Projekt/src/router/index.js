608651d1556f9857be1dad6726dbe6a0a4894bad
Emma Forslund <emfo2102@student.miun.se>
2022-10-18 23:33:01 +0200

    Skapat routes

--- Projekt/src/router/index.js
+++ Projekt/src/router/index.js
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import Categories from "../views/CategoryView.vue";
+import Products from "../views/ProductView.vue";
+import SingleProduct from "../views/SingleProductView.vue";
+import AddProduct from "../views/AddProductView.vue";
+import SingleCategory from "../views/SingleCategoryView.vue";
+import AddCategory from "../views/AddCategoryView.vue";
+import SearchView from "../views/SearchView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +15,45 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
-    }
+    },
+    {
+      path: "/categories",
+      name: "categories",
+      component: Categories,
+    },
+    {
+      path: "/products",
+      name: "products",
+      component: Products,
+    },
+    {
+      path: '/product/:id',
+      name: 'singleproduct',
+      component: SingleProduct,
+      props: true
+    },
+    {
+      path: "/addproduct",
+      name: "addproduct",
+      component: AddProduct,
+    },
+    {
+      path: '/category/:id',
+      name: 'singlecategory',
+      component: SingleCategory,
+      props: true
+    },
+    {
+      path: "/addcategory",
+      name: "addcategory",
+      component: AddCategory
+    },
+    {
+      path: '/result/:name',
+      name: 'search',
+      component: SearchView,
+      props: true
+    },
   ],
 });
 

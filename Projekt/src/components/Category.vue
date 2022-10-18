f944e295f4b1a15c19f563604f041c6628b956fa
Emma Forslund <emfo2102@student.miun.se>
2022-10-18 23:30:24 +0200

    Skapat komponenter för produkt och kategori

---
+++ Projekt/src/components/Category.vue
@@ -0,0 +1,28 @@
+<template>
+    <article class="p-5 mt-8">
+        <div class="flex justify-between bg-medium-color rounded-t-lg">
+            <div>
+                <h2 class="font-headings text-xxl text-white px-2">
+                    {{category.categoryname}}</h2>
+            </div>
+            <div class="p-3 text-white cursor-pointer flex">
+                <Router-link class="text-white" :to="{name: 'singlecategory', params: { id: category.id } }"><i
+                        class="fa-xl fa-solid fa-pen"></i></Router-link>
+                </div>
+            </div>
+        <p class="font-content text-base px-2 py-3 shadow shadow-gray-400">{{category.categorydescription}}</p>
+    </article>
+</template>
+
+<script>
+export default {
+    props: {
+        category: Object
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ Projekt/src/components/Product.vue
@@ -0,0 +1,31 @@
+<template>
+    <tr class="border-solid border-2 border-light-pink font-content hover:bg-slate-100">
+        <td>{{product.id}}
+        </td>
+        <td>{{product.name}}</td>
+        <td>{{product.quantity}}
+        </td>
+        <td>{{product.price}}</td>
+        <td>{{product.categoryname}}</td>
+        <td>
+            <Router-link class="text-dark-color" :to="{name: 'singleproduct', params: { id: product.id } }"><i
+                    class="fa-xl fa-solid fa-pen"></i></Router-link>
+        </td>
+        <td @click="$emit('delete')"><i class="fa-xl fa-solid fa-trash cursor-pointer text-dark-color"></i></td>
+    </tr>
+</template>
+
+<script>
+export default {
+    props: {
+        product: Object
+    }
+}
+
+</script>
+
+<style scoped>
+td {
+    padding: 1em;
+}
+</style>

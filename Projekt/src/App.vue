b8ef74f731ebbdf55eb4bba2485d88394c5d1622
Emma Forslund <emfo2102@student.miun.se>
2022-10-18 23:29:23 +0200

    Skapat App.vue

--- Projekt/src/App.vue
+++ Projekt/src/App.vue
@@ -1,11 +1,30 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from "vue-router";
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+
+
+export default {
+    components: {
+        Header,
+        RouterView,
+        Footer
+    }
+}
 
 </script>
 
 <template>
 
+    <Header />
+    <main>
+        <RouterView />
+    </main>
+    <Footer />
 </template>
+
+<script>
+</script>
 
 <style scoped>
 

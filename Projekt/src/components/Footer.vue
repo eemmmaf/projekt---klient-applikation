dacdd73af7821eebc233498e8b5ba0478fe29fba
Emma Forslund <emfo2102@student.miun.se>
2022-10-18 23:31:13 +0200

    Skapat Footer och Header-komponent

---
+++ Projekt/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer class="bg-main-color mt-11">
+        <div class="flex flex-row text-white justify-center">
+            <div>
+                <h2>Hälsolagret</h2>
+                <p>BLalblalablba</p>
+            </div>
+            <div>
+                <h2>Hälsolagret</h2>
+                <p>BLalblalablba</p>
+            </div>
+            <div>
+                <h2>Hälsolagret</h2>
+                <p>BLalblalablba</p>
+            </div>
+        </div>
+
+    </footer>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
---
+++ Projekt/src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+    <header class="border-b-2 shadow-s">
+        <div class="flex justify-between">
+            <div class="flex">
+                <div class="p-3 absolute left-40 mt-2">
+                    <router-link to="/">
+                        <h1 class="font-headings text-xl p-1 font-bold ml-10 text-dark-color">Matbolaget</h1>
+                    </router-link>
+                </div>
+            </div>
+            <div class="flex border-l-2">
+                <div class="m-8 cursor-pointer text-dark-color">
+                    <!--Sökruta-->
+                        <Searchbox />
+                </div>
+
+                <!--Cirkel istället för bild-->
+                <div class="self-center border-l-2 p-3 text-dark-color">
+                    <div class="circle">
+                    </div>
+                    <!--Inloggades namn-->
+                    <p class="font-content font-bold">Emma Forslund</p>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+<script>
+import Searchbox from '../components/Searchbox.vue';
+
+import { RouterLink } from 'vue-router';
+
+export default {
+    components: {
+        Searchbox,
+        RouterLink
+    },
+    data() {
+        return {
+            toggle: false,
+            result: [],
+            products: ""
+        }
+    }
+}
+
+
+</script>
+
+<style scoped>
+img {
+    max-width: 100px;
+    margin-top: 0.5em;
+}
+
+.circle {
+    background-color: #C080E2;
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    margin-left: 2rem;
+}
+</style>

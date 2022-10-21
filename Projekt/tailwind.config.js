7e6bb786ce704bedbcc32807886947f1eb52801a
Emma Forslund <emfo2102@student.miun.se>
2022-10-21 13:05:10 +0200

    Lagt till en breakpoint

--- Projekt/tailwind.config.js
+++ Projekt/tailwind.config.js
@@ -4,35 +4,38 @@ const plugin = require('tailwindcss/plugin')
 
 
 module.exports = {
-  content:[
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx,vue}"
   ],
   theme: {
     extend: {
       colors: {
-        'main-color' : '#E8C3FE',
+        'main-color': '#E8C3FE',
         'dark-color': '#1E012D',
-        'medium-color' : '#8101C6;',
+        'medium-color': '#8101C6;',
         'light-color': '#C080E2',
         'error': '#A80000'
       },
       fontFamily: {
         'headings': ['Manrope', 'Open-Sans', 'sans-serif'],
-        'content': ['Open-sans','sans-serif']
-    },
-    container: {
-      center: true,
-    },
-    fontSize: {
-      sm: '1rem',
-      base: '1.1rem',
-      xl: '1.7rem',
-      xxl: '2rem'
+        'content': ['Open-sans', 'sans-serif']
+      },
+      container: {
+        center: true,
+      },
+      fontSize: {
+        sm: '1rem',
+        base: '1.1rem',
+        xl: '1.7rem',
+        xxl: '2rem'
+      },
+      screens: {
+        'medium': '930px'
+      },
+      plugins: [
+        require('@tailwindcss/forms')
+      ],
     }
-  },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
-}
+  }
 }

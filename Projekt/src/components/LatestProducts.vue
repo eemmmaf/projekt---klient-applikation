04d0562d62f2c695b1b81238658dda409ab0f24e
Emma Forslund <emfo2102@student.miun.se>
2022-10-24 15:55:01 +0200

    Skapat komponent där 5 senaste produkter visas

---
+++ Projekt/src/components/LatestProducts.vue
@@ -0,0 +1,16 @@
+<template>
+<ul><li class="font-content text-base mt-2 text-center md:text-left"> <Router-link class="text-dark-color underline hover:text-medium-color" :to="{name: 'singleproduct', params: { id: product.id } }">{{product.name}}</Router-link></li></ul>
+
+</template>
+
+<script>
+export default {
+    props: {
+        product: Object
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

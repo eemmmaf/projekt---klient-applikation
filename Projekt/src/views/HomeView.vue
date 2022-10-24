f3e5d43d77af33e3b4fbecac510d8c8dfb9c06ea
Emma Forslund <emfo2102@student.miun.se>
2022-10-24 15:55:13 +0200

    Skriver ut innehåll på startsidan

--- Projekt/src/views/HomeView.vue
+++ Projekt/src/views/HomeView.vue
@@ -13,29 +13,57 @@
                 </div>
             </div>
 
-            <!--Antal varor-->
-            <div class="border-solid border-2 border-light-pink p-2 rounded-2xl w-1/4 text-center">
-                <h3>Lagrade produkter</h3>
-                <p class="font-bold text-xxl">{{result}}</p>
+
+            <div class="flex justify-between flex-col md:flex-row">
+                <!--Antal varor-->
+                <div class="">
+                    <div
+                        class="border-solid border-2 border-light-pink p-2 bg-medium-color text-white mb-3 mt-2 rounded-2xl text-center">
+                        <h3>Lagrade produkter</h3>
+                        <p class="font-bold text-xxl">{{result}}st</p>
+                    </div>
+                    <div
+                        class="border-solid border-2 border-light-pink p-2 bg-main-color text-dark-color mt-2 mb-3 rounded-2xl text-center">
+                        <h3>Lagrade kategorier</h3>
+                        <p class="font-bold text-xxl">{{catResult}}st</p>
+                    </div>
+
+                    <div class="border-solid border-2 border-light-pink p-5 mt-4">
+                        <h2 class="font-headings text-xl text-center md:text-left font-bold">5 senaste produkterna
+                        </h2>
+                        <LatestProducts v-for="product in products.slice(0, 5)" :product="product" :key="product.id" />
+                    </div>
+                </div>
+
+
+                <div>
+                    <Userinfo />
+                </div>
+
             </div>
         </div>
+
     </div>
 </template>
 
 <script>
-import Boxes from '../components/Boxes.vue';
+import Userinfo from '../components/Userinfo.vue';
+import LatestProducts from '../components/LatestProducts.vue';
 
 export default {
     data() {
         return {
             result: "",
             products: [],
+            categories: [],
             quantity: "",
-            quantityFive: ""
+            catResult: "",
+            created_at: ""
         }
     },
     components: {
-        Boxes
+        Userinfo,
+        LatestProducts
     },
     methods: {
         currentDate() {
@@ -56,13 +84,34 @@ export default {
 
             const data = await resp.json();
             this.products = data;
+
+
+            this.products.sort(function (a, b) {
+                return b.id - a.id;
+            });
+
+
             //Räknar hur många produkter som är lagrade
             this.result = this.products.length;
-
+        },
+        //Hämtar alla kategorier
+        async getCategories() {
+            this.token = localStorage.getItem('token');
+            const resp = await fetch("http://localhost:8000/api/getcategories", {
+                method: 'GET',
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": "Bearer " + this.token
+                }
+            });
+            const data = await resp.json();
+            this.categories = data;
+            this.catResult = this.categories.length;
         },
     },
     mounted() {
         this.getProducts();
+        this.getCategories();
     }
 }
 </script>
